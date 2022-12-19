1f17901c8fd215beda48163900b7df5ea7a231ff
ealtugkirman <110311214+ealtugkirman@users.noreply.github.com>
2022-12-19 16:32:50 +0300

    Update Header.js
    margin fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className="bg-first text-center text-xs lg:text-sm   xl:text-base text-white p-1">
-        <p className='mr-24 ml-24' >
+        <p className='mr-8 ml-8' >
         "Welcome to our website, where we collect all of Kanye West's most outrageous and ridiculous statements and present them to you for your amusement. Every time you click, you'll be treated to a new gem of absurdity, pulled from the depths of Kanye's ego and inflated sense of self-importance.
         </p>
     </div>
